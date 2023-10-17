27d4d9f8b552119e4a983129f3d7bb2f4e43915a
301-14 <angortizal@uch.pe>
2023-10-16 19:54:29 -0500

    Version2(actualizado)

---
+++ js/myapp.js
@@ -0,0 +1,72 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    fetch('login.json')
+    .then(response => response.json())
+    .then(data => {
+        if (username === data.username && password === data.password) {
+        window.location.href = 'dashboard.html';
+        localStorage.setItem('isLoggedIn', 'true');
+        } else {
+        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+        }
+    });
+}
+
+function getUserData() {
+    fetch('userdata.json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('userName').textContent = data.name;
+        document.getElementById('accountNumber').textContent = data.accountNumber;
+        document.getElementById('balance').textContent = data.balance;
+    });
+}
+
+function getTransactions() {
+    fetch('transactions.json')
+    .then(response => response.json())
+    .then(data => {
+        const transactionList = document.getElementById('transactionList');
+        data.forEach(transaction => {
+        const li = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = '#';
+        link.textContent = `Transacción ${transaction.id}`;
+        link.onclick = function() {
+            localStorage.setItem('transactionId', transaction.id);
+            window.location.href = 'transactiondetails.html';
+        };
+        li.appendChild(link);
+        transactionList.appendChild(li);
+        });
+    });
+}
+
+function getTransactionDetails() {
+    const transactionId = localStorage.getItem('transactionId');
+    if (transactionId) {
+    fetch('transactiondetails.json')
+        .then(response => response.json())
+        .then(data => {
+        const transaction = data;
+        document.getElementById('amount').textContent = transaction.amount;
+        document.getElementById('date').textContent = transaction.date;
+        document.getElementById('code').textContent = transaction.code;
+        document.getElementById('description').textContent = transaction.description;
+        });
+    }
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (isLoggedIn) {
+    getUserData();
+    getTransactions();
+    const dashboardPage = document.getElementById('dashboardPage');
+    if (dashboardPage) {
+        getTransactionDetails();
+    }
+    } else {
+    window.location.href = 'index.html';
+    }
+});
